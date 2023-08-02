d18cf0f0f5071217a9575de7461b43b4ecc6c2ef
BMA <benjammartin@gmail.com>
2023-08-02 08:59:15 +0200

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -9,42 +9,56 @@ function App() {
     <React.Fragment>
       <Tabs.Root defaultValue="1">
         <Tabs.List className={styles.list}>
-          <Tabs.Trigger value="1" className={styles.trigger}>
-            <span className={styles.label}>Tab example</span>
-            <Button>
-              <KebabIcon />
-            </Button>
-          </Tabs.Trigger>
-          <Tabs.Trigger value="2" className={styles.trigger}>
-            <span className={styles.label}>Tab example</span>
-            <Button>
-              <KebabIcon />
-            </Button>
-          </Tabs.Trigger>
-          <Tabs.Trigger value="3" className={styles.trigger}>
-            <span className={styles.label}>Tab example</span>
-            <Button>
-              <KebabIcon />
-            </Button>
-          </Tabs.Trigger>
-          <Tabs.Trigger value="4" className={styles.trigger}>
-            <span className={styles.label}>Tab example</span>
-            <Button>
-              <KebabIcon />
-            </Button>
-          </Tabs.Trigger>
-          <Tabs.Trigger value="5" className={styles.trigger}>
-            <span className={styles.label}>Tab example</span>
-            <Button>
-              <KebabIcon />
-            </Button>
-          </Tabs.Trigger>
-          <Tabs.Trigger value="6" className={styles.trigger}>
-            <span className={styles.label}>Tab example</span>
-            <Button>
-              <KebabIcon />
-            </Button>
-          </Tabs.Trigger>
+          <div className={styles.scroller}>
+            <Tabs.Trigger value="1" className={styles.trigger}>
+              <span className={styles.label}>Tab example</span>
+              <Button>
+                <KebabIcon />
+              </Button>
+            </Tabs.Trigger>
+            <Tabs.Trigger value="2" className={styles.trigger}>
+              <span className={styles.label}>Tab example</span>
+              <Button>
+                <KebabIcon />
+              </Button>
+            </Tabs.Trigger>
+            <Tabs.Trigger value="3" className={styles.trigger}>
+              <span className={styles.label}>Tab example</span>
+              <Button>
+                <KebabIcon />
+              </Button>
+            </Tabs.Trigger>
+            <Tabs.Trigger value="4" className={styles.trigger}>
+              <span className={styles.label}>Tab example</span>
+              <Button>
+                <KebabIcon />
+              </Button>
+            </Tabs.Trigger>
+            <Tabs.Trigger value="5" className={styles.trigger}>
+              <span className={styles.label}>Tab example</span>
+              <Button>
+                <KebabIcon />
+              </Button>
+            </Tabs.Trigger>
+            <Tabs.Trigger value="6" className={styles.trigger}>
+              <span className={styles.label}>Tab example</span>
+              <Button>
+                <KebabIcon />
+              </Button>
+            </Tabs.Trigger>
+            <Tabs.Trigger value="7" className={styles.trigger}>
+              <span className={styles.label}>Tab example</span>
+              <Button>
+                <KebabIcon />
+              </Button>
+            </Tabs.Trigger>
+            <Tabs.Trigger value="8" className={styles.trigger}>
+              <span className={styles.label}>Tab example</span>
+              <Button>
+                <KebabIcon />
+              </Button>
+            </Tabs.Trigger>
+          </div>
         </Tabs.List>
         <Tabs.Content value="1" className={styles.content}>
           Tabs 1
@@ -62,6 +76,12 @@ function App() {
           Tabs 5
         </Tabs.Content>
         <Tabs.Content value="6" className={styles.content}>
+          Tabs 6
+        </Tabs.Content>
+        <Tabs.Content value="7" className={styles.content}>
+          Tabs 6
+        </Tabs.Content>
+        <Tabs.Content value="8" className={styles.content}>
           Tabs 6
         </Tabs.Content>
       </Tabs.Root>
